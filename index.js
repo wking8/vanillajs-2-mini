bc981a2c646eb6ccd185942cd77b701b53fa7b6d
Wyatt King <wyattking@Wyatts-MacBook-Pro.local>
2019-07-03 13:44:19 -0600

    save dis

---
+++ index.js
@@ -0,0 +1,27 @@
+document.querySelector("form").addEventListener("submit", addTodo);
+
+function addTodo(event) {
+    event.preventDefault();
+    const item = document.createElement("li");
+    item.innerText = document.getElementById("item").value;
+  
+    const button = document.createElement("button");
+    button.innerText = "x";
+    button.addEventListener("click", removeTodo);
+    item.append(button);
+  
+    const list = document.querySelector("ul");
+    list.appendChild(item);
+  }
+  
+  function removeTodo(event) {
+    event.target.parentNode.remove();
+  }
+  function completeTodo(event) {
+    const value = event.target.getAttribute("aria-checked");
+    if (value !== "true") {
+      event.target.setAttribute("aria-checked", "true");
+    } else {
+      event.target.setAttribute("aria-checked", "false");
+    }
+  }
